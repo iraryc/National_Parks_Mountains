ec28fdf1a49102dd9ca0eb5a8b0c53df7f63eece
Andrea Ceballos <iraryc@gmail.com>
2024-05-22 19:39:47 -0700

    got the type to display all the information and worked on my mountain page
    got the type to display all the information and worked on my mountain page

--- JavaScript/mountains.js
+++ JavaScript/mountains.js
@@ -1,53 +1,65 @@
-"use strict"
+"use strict";
 
-function mountaindropdown(optionsMounArray, defaultText){
-    const dropdown = document.getElementById("mountainsDropdown");
-    if(!dropdown){
-        console.error("Dropdown element not found");
-        return;
-    }
-   dropdown.innerHTML="";
+function mountaindropdown(optionsMounArray, defaultText) {
+  const dropdown = document.getElementById("mountainsDropdown");
+  if (!dropdown) {
+    console.error("Dropdown element not found");
+    return;
+  }
+  dropdown.innerHTML = "";
 
-   const defaultOption = document.createElement("option");
-   defaultOption.value="";
-   defaultOption.textContent = defaultText;
-   defaultOption.disabled = true;
-   defaultOption.selected = true;
-   dropdown.appendChild(defaultOption);
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = defaultText;
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  dropdown.appendChild(defaultOption);
 
-   optionsMounArray.forEach(option => {
+  optionsMounArray.forEach((option) => {
     const createNewOption = document.createElement("option");
     createNewOption.value = option.name;
     createNewOption.textContent = option.name;
     dropdown.appendChild(createNewOption);
-   })
-};
+  });
+}
 
+function setupMountainDropDown() {
+  const mountainDropDownSection = document.getElementById(
+    "mountainDropDownSection"
+  );
+  mountainDropDownSection.style.display = "block";
 
-function setupMountainDropDown(){
+  mountaindropdown(mountainsArray, "Select Mountain Information");
 
-    const mountainDropDownSection = document.getElementById("mountainDropDownSection");
-    mountainDropDownSection.style.display = 'block';
-    
+  document
+    .getElementById("mountainsDropdown")
+    .addEventListener("change", function () {
+      const selectedMountain = mountainsArray.find(
+        (mountain) => mountain.name === this.value
+      );
 
-    mountaindropdown(mountainsArray, "Select Mountain Information");
-
-    document.getElementById("mountainsDropdown").addEventListener('change', function(){
-        const selectedMountain  = mountainsArray.find(mountain => mountain.name === this.value);
-        
-        
-        if (selectedMountain) {
-            document.getElementById("displayParks").style.display = 'block';
-            document.getElementById("parkImageDisplay").src = selectedMountain.img;
-            document.getElementById("cardTitle").textContent = selectedMountain.name;
-            document.getElementById("cardAddress").textContent = `Elevation: ${selectedMountain.elevation} ft`;
-            document.getElementById("cardContact").textContent = `Effort: ${selectedMountain.effort}`;
-            document.getElementById("cardLat&Long").textContent = `Coordinates: ${selectedMountain.coords.lat}, ${selectedMountain.coords.lng}`;
-            document.getElementById("cardWebsite").textContent = selectedMountain.desc;
-        }
+      if (selectedMountain) {
+        const mountainCard = document.getElementById("displayParks");
+        mountainCard.innerHTML = 
+        `<div class="card mb-3 cardmountain" id="displayParks">
+            <img src="${selectedMountain.img}" class="card-img-top mountainimage justify-content-center" alt="" id="parkImageDisplay">
+            <div class="card-body">
+            <h5 class="card-title" id="cardTitle">${selectedMountain.name}</h5>
+            <p class="card-text" id="cardAddress">${selectedMountain.elevation}</p>
+            <p class="card-text" id="cardContact">${selectedMountain.effort}</p>
+            <p class="card-text" id="cardLat&Long">${selectedMountain.desc}</p>
+            <p class="card-text" id="cardWebsite">${selectedMountain.elevation}</p>
+            </div>
+        </div>`;
+        // document.getElementById("displayParks").style.display = 'block';
+        // document.getElementById("parkImageDisplay").src = selectedMountain.img;
+        // document.getElementById("cardTitle").textContent = selectedMountain.name;
+        // document.getElementById("cardAddress").textContent = `Elevation: ${selectedMountain.elevation} ft`;
+        // document.getElementById("cardContact").textContent = `Effort: ${selectedMountain.effort}`;
+        // document.getElementById("cardLat&Long").textContent = `Coordinates: ${selectedMountain.coords.lat}, ${selectedMountain.coords.lng}`;
+        // document.getElementById("cardWebsite").textContent = selectedMountain.desc;
+      }
     });
+}
 
-   
-};
-
-document.addEventListener('DOMContentLoaded', setupMountainDropDown);
+document.addEventListener("DOMContentLoaded", setupMountainDropDown);
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -38,89 +38,40 @@ function setupDropDown(){
     document.getElementById("parksDropdown").addEventListener('change', displayParks)
 };
 
-function displayParks(){
+
+function displayParks() {
     const selectedValue = document.getElementById("parksDropdown").value;
-    const filteredParks = nationalParksArray.filter(park => park.State === selectedValue);
-    // const filterHistoricTypePark = parkTypesArray.filter(parkType => parkType.LocationName.includes("Historic"));
-    
+
+    let filteredParks = [];
+    if (parkTypesArray.indexOf(selectedValue) > -1) {
+        filteredParks = nationalParksArray.filter(park => park.LocationName.includes(selectedValue));
+    } else if (locationsArray.indexOf(selectedValue) > -1) {
+        filteredParks = nationalParksArray.filter(park => park.State === selectedValue);
+    }
+
     const displayCard = document.getElementById("displayParks");
     displayCard.innerHTML = "";
-    
+
     filteredParks.forEach(park => {
-        const card = 
-        // `<div class="card-body">
-        //     <h5 class="card-title" id="cardTitle">${park.LocationName}</h5>
-        //     <p class="card-text" id="cardAddress">${park.Address} ${park.City} ${park.State} ${park.ZipCode}</p>
-        //     <p class="card-text" id="cardContact">${park.Phone} <br></br> ${park.Fax}</p>
-        //     <p class="card-text" id="cardLat&Long">${park.Latitude} and ${park.Longitude}</p>
-        //     <p class="card-text" id="cardWebsite">${park.Visit}</p>
-        //   </div>`
-
-        //   `<table class="table">
-        //   <thead>
-        //     <tr>
-        //       <th scope="col">ID</th>
-        //       <th scope="col">Location Name</th>
-        //       <th scope="col">Address</th>
-        //       <th scope="col">Contact</th>
-        //       <th scope="col">URL</th>
-        //     </tr>
-        //   </thead>
-        //   <tbody>
-        //     <tr>
-        //       <th scope="row">${park.LocationID}</th>
-        //       <td>${park.LocationName}</td>
-        //       <td>${park.Address} ${park.City} ${park.State} ${park.ZipCode}</td>
-        //       <td>${park.Phone} <br></br> ${park.Fax}</td>
-        //       <td>${park.Visit}</td>
-        //     </tr>
-        //   </tbody>
-        // </table>`
-
-        `<div class="col-lg-4 col-sm-6">
-        <div class="nationalpark">
-          <img class="myaboutimages" src="/images/grandcanyonpixbay.jpg" alt="adamsstory">
-          <div class="overlay">
-            <div>
-              <h4 class="text-white">${park.LocationID}</h4>
-              <h4 class="text-white">${park.LocationName}</h4>
-              <p class="text-white">${park.Address} ${park.City} ${park.State} ${park.ZipCode}</p>
-              <p class="text-white">${park.Phone} | ${park.Fax}</p>
-              <p class="text-white">${park.Visit}</p>
-            </div>
-          </div>
-        </div>
-      </div>`
-
-          displayCard.innerHTML += card; 
+        const card = `
+            <div class="col-lg-4 col-sm-6">
+                <div class="nationalpark">
+                    <img class="myaboutimages" src="/images/grandcanyonpixbay.jpg" alt="Park Image">
+                    <div class="overlay">
+                        <div>
+                            <h4 class="text-white">${park.LocationID}</h4>
+                            <h4 class="text-white">${park.LocationName}</h4>
+                            <p class="text-white">${park.Address}, ${park.City}, ${park.State}, ${park.ZipCode}</p>
+                            <p class="text-white">${park.Phone} | ${park.Fax}</p>
+                            <p class="text-white"><a href="${park.Visit}" target="_blank" class="text-white">Visit Website</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+        displayCard.innerHTML += card;
     });
-       
-    // filterHistoricTypePark.forEach(parkType => {
-    //     const historiccard = 
-    //     `<div class="col-lg-4 col-sm-6">
-    //     <div class="nationalpark">
-    //       <img class="myaboutimages" src="/images/grandcanyonpixbay.jpg" alt="adamsstory">
-    //       <div class="overlay">
-    //         <div>
-    //           <h4 class="text-white">${park.LocationID}</h4>
-    //           <h4 class="text-white">${park.LocationName}</h4>
-    //           <p class="text-white">${park.Address} ${park.City} ${park.State} ${park.ZipCode}</p>
-    //           <p class="text-white">${park.Phone} | ${park.Fax}</p>
-    //           <p class="text-white">${park.Visit}</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>`
-    //   displayCard.innerHTML += historiccard; 
-
-
-    // })
-        
-        
-    //user innerHTML +- park
-
-
 }
+
 
 document.addEventListener('DOMContentLoaded', setupDropDown);
 
